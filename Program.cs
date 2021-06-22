2d6ffdf3a4dbacc88c3b1e394842fe1d9ebddee8
Haraaca Technologies peter <peternjuguna76@gmail.com>
2021-06-22 13:19:53 +0300

    update

--- Program.cs
+++ Program.cs
@@ -6,6 +6,12 @@ namespace NewRepo
     {
         static void Main(string[] args)
         {
+            var cs = @"Server=localhost\SQLEXPRESS;Database=etl;Trusted_Connection=True;";
+
+            using var con = new SqlConnection(cs);
+            con.Open();
+        }
+        {
             Console.WriteLine("Hello World!");
         }
     }
