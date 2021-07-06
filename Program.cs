26473a8651ce66d618fa13deae2332abe76fd4a1
Haraaca Technologies peter <peternjuguna76@gmail.com>
2021-07-06 13:57:20 +0300

    main introduced

--- Program.cs
+++ Program.cs
@@ -1,40 +1,50 @@
-﻿using System;
-using System.io;
-using system.text;
-
-
-static void Main(string[] args);
+﻿using System.IO;
+using System.Text;
 
 
 namespace NewRepo
 {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            read myClass = new read();
+            myClass.rea("some string");
+            StreamReader reader = new StreamReader(fileName);
+            //streamreader => StreamReader
+            // your code
+}
+}
    class read
     {
-        public static string read(string files, string)
+        public static string rea(string files)
         {
+            
             using (streamreader sr = files.openText(@"C:\Users\HP\Desktop\materials\products.tsv"))
             {
-                string S;
+                string s;
                 while ((s = sr.ReadLine()) != null)
 
                 {   
-                    Console.WriteLine(S);
+                    Console.WriteLine(s);
 
                 }
+                sr.Close();
 
             }
         }
-    }
-
+      
     
         
-}
+        }
+    }
         
     
 
 
 
        // static void Main(string[] args)
+       //static void main (string[] args)
         //{
             //var cs = @"Server=localhost\SQLEXPRESS;Database=etl;Trusted_Connection=True;";
 
